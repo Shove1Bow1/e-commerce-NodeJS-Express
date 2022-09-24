b49eb1541b6936e48784c982f5ad5d7897efb8ba
NguyenWhamyTech <110946003+NguyenWhamyTech@users.noreply.github.com>
2022-09-24 16:29:13 +0700

    update 24/9 add config mongodb

---
+++ service/MongoDB.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+var MongoClient = require("mongodb").MongoClient;
+const client = new MongoClient(process.env.MOGODBCONNECT);
+const Client = (collection) => {
+  return client.db("Main").collection(collection);
+};
+export default Client;
