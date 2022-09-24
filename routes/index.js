d5febbfb5279e8c5f80b8d3db7dcd060ae81d596
NguyenWhamyTech <110946003+NguyenWhamyTech@users.noreply.github.com>
2022-09-24 16:19:18 +0700

    update 24/9 config mogodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+var MongoClient = require("mongodb").MongoClient;
+const client = new MongoClient(
+  "mongodb+srv://ECommerceApp:slowly123@education-project.5aw2yco.mongodb.net/test"
+);
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-
+router.post("/category", function (req, res, next) {
+  const { name } = req.body;
+  console.log(req.body);
+  // client.db("test").collection("devices").insertOne({ temp: name });
+});
 module.exports = router;
