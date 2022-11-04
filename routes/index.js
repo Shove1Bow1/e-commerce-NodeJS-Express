94c3761c10562d487afc816bc74281933c746c0c
Shov1bow1 <fireblaster2001@gmail.com>
2022-11-05 01:07:31 +0700

    remove id and update api user

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
 var express = require("express");
 var router = express.Router();
 require("dotenv").config();
-var {ShipementSchema}=require('../service/schemas/shipment_schema')
 /* GET home page. */
 router.get("/some", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 router.post("/category",async function (req, res, next) {
   const { name } = req.body;
-  let doc=await ShipementSchema.create({billId:"77878",isDelete:0});
-  await doc.save();
-  console.log(doc.createdAt);
-  res.send("ok");
+  // let doc=await ShipementSchema.create({billId:"77878",isDelete:0});
+  // await doc.save();
+  // console.log(doc.createdAt);
+  // res.send("ok");
   // client.db("test").collection("devices").insertOne({ temp: name });
 });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var uuidConverter = require('uuid');
-const crypto=require("crypto");
+const crypto = require("crypto");
 var { Users } = require("../service/collections/users");
 var { AcceptIncomingReq, GenerateRecoverCode } = require("../service/function_config");
 const { CheckExist } = require("../ultis/checkExistUser");
@@ -11,27 +11,37 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 router.post("/register", AcceptIncomingReq, CheckExist, async (req, res) => {
-  if(!req.body.userName || !req.body.password || !req.body.phoneNumber || !req.body.address || !req.body.email){
-    res.send({status:"missing some info",message:false,messageResponse:messageRespone("400")});
+  if (!req.body.userName || !req.body.password || !req.body.phoneNumber || !req.body.address || !req.body.email) {
+    res.send({ status: "missing some info", message: false, messageResponse: messageRespone("400") });
     return;
   }
   const { userName, password, phoneNumber, address, email } = req.body;
-  const emailConvert=email.toLowerCase();
+  const emailConvert = email.toLowerCase();
   const pwHex = crypto.createHash("sha256").update(password).digest("hex");
-  const idUser = uuidConverter.v1(userName);
+  // const idUser = uuidConverter.v1(userName);
   const codeRecover = GenerateRecoverCode(5);
-  let doc = await Users.create({ idUser, email: emailConvert, userName: userName, password: pwHex, address: address, roles: "user", phoneNumber: phoneNumber, secretKey: codeRecover });
+  let doc = await Users.create({ email: emailConvert, userName: userName, password: pwHex, address: address, roles: "user", phoneNumber: phoneNumber, secretKey: codeRecover });
   doc.save();
+  await Users.findOne({ email: emailConvert },async (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    if (result){
+      req.id =await result._id.toString();
+    }
+      
+  }).clone();
   if (doc.createdAt) {
     try {
+      console.log(req.id)
       let dateCreated = doc.createdAt[8] + doc.createdAt[9] + '/' + doc.createdAt[5] + doc.createdAt[6] + '/' + doc.createdAt[0] + doc.createdAt[1] + doc.createdAt[2] + doc.createdAt[3];
       FirstRegisterSender(userName, dateCreated, email, codeRecover);
       res.send({
-        message:true,
-        status:"welcome new user",
-        idUser:userId,
-        userName:userName,
-        messageRespone:messageRespone("200"),
+        message: true,
+        status: "welcome new user",
+        idUser: req.id,
+        userName: userName,
+        messageRespone: messageRespone("200"),
       })
       return
     }
@@ -40,12 +50,12 @@ router.post("/register", AcceptIncomingReq, CheckExist, async (req, res) => {
     }
   }
   else {
-    res.send({status:"failed",messageRespone:messageRespone["400"]});
+    res.send({ status: "failed", messageRespone: messageRespone["400"] });
   }
 })
 router.post("/login", AcceptIncomingReq, async (req, res) => {
-  if(!req.body.email || !req.body.password){
-    res.send({status:"missing some info",message:false,messageResponse:messageRespone("400")});
+  if (!req.body.email || !req.body.password) {
+    res.send({ status: "missing some info", message: false, messageResponse: messageRespone("400") });
     return;
   }
   const { email, password } = req.body;
@@ -55,92 +65,95 @@ router.post("/login", AcceptIncomingReq, async (req, res) => {
       console.log(err)
     }
     if (!result) {
-      req.checked = true; 
-      res.send({status:"failed to login",messageRespone:messageRespone("400") });
+      req.checked = true;
+      res.send({ status: "failed to login", messageRespone: messageRespone("400") });
     }
     else {
       res.send(
         {
           status: "welcome user",
-          idUser: result.idUser,
-          userName:result.userName,
-          messageRespone:messageRespone("200"),
+          idUser: result._id.toString(),
+          userName: result.userName,
+          messageRespone: messageRespone("200"),
         });
       return;
     }
   }).clone();
-   
+
 })
-router.post("/recover", AcceptIncomingReq,async (req, res) => {
-  if(!req.body.email || !req.body.secretCode){
-    res.send({status:"missing some info",message:false,messageResponse:messageRespone("400")});
+router.post("/recover", AcceptIncomingReq, async (req, res) => {
+  if (!req.body.email || !req.body.secretCode) {
+    res.send({ status: "missing some info", message: false, messageResponse: messageRespone("400") });
     return;
   }
   const { email, secretCode } = req.body;
-  const emailConvert=email.toLowerCase();
+  const emailConvert = email.toLowerCase();
   await Users.findOne({ email: emailConvert, secretKey: secretCode }, (err, result) => {
     if (err) {
       console.log(err);
     }
-    if (!result){
+    if (!result) {
       req.checked = true
     }
-    else{
+    else {
       res.send(
         {
-          status:"user exists",
+          status: "user exists",
           message: true,
-          idUser: result.idUser,
-          messageRespone:messageRespone("200"),
+          idUser: result._id,
+          messageRespone: messageRespone("200"),
         }
       );
     }
-     
+
   }).clone();
   if (req.checked)
     res.send(
       {
         status: "user not exists",
-        message:false,
+        message: false,
         messageRespone: messageRespone("400")
       }
     )
 })
 router.post("/forgot_password", AcceptIncomingReq, async (req, res) => {
-  if(!req.body.uuid || !req.body.newPassword){
-    res.send({status:"missing some info",message:false,messageResponse:messageRespone("400")});
+  if (!req.body.uuid || !req.body.newPassword) {
+    res.send({ status: "missing some info", message: false, messageResponse: messageRespone("400") });
     return;
   }
   const { uuid, newPassword } = req.body;
   const pwHex = crypto.createHash("sha256").update(newPassword).digest("hex");
-  var result =await Users.updateOne({ idUser: uuid }, { $set: { password: pwHex } }, (err) => {
+  var result = await Users.updateOne({ _id: uuid }, { $set: { password: pwHex } }, (err) => {
     if (err) {
       console.log(err);
       res.send(messageRespone[400]);
     }
   }).clone();
-  if(result.modifiedCount>0){
-    res.send({status:"update success",isUpdate:true,response:messageRespone("200")})
+  if (result.modifiedCount > 0) {
+    res.send({ status: "update success", isUpdate: true, response: messageRespone("200") })
   }
-  else{
-    res.send({status:"update failed",isUpdate:false,response:messageRespone("400")})
+  else {
+    res.send({ status: "update failed", isUpdate: false, response: messageRespone("400") })
   }
 })
-router.post("/update_password",AcceptIncomingReq,async(req,res)=>{
-  const{uuid,newPassword,oldPassword}=req.body;
-  const oldPwHex=crypto.createHash("sha256").update(oldPassword).digest("hex");
-  const newPwHex=crypto.createHash("sha256").update(newPassword).digest("hex");
-  var result=await Users.updateOne({idUser:uuid,password:oldPwHex},{$set:{password:newPwHex}},(err)=>{
-    if(err)
-    {
+router.post("/update_password", AcceptIncomingReq, async (req, res) => {
+  if (!req.body.uuid || !req.body.newPassword || !req.body.oldPassword) {
+    res.send({ status: "missing some info", message: false, messageResponse: messageRespone("400") });
+    return;
+  }
+  const { uuid, newPassword, oldPassword } = req.body;
+  const oldPwHex = crypto.createHash("sha256").update(oldPassword).digest("hex");
+  const newPwHex = crypto.createHash("sha256").update(newPassword).digest("hex");
+  var result = await Users.updateOne({ _id: uuid, password: oldPwHex }, { $set: { password: newPwHex } }, (err) => {
+    if (err) {
       console.log(err);
     }
   }).clone();
-  if(result.modifiedCount>0){
-    res.send({status:"update success",isUpdate:true,response:messageRespone("200")})
+  if (result.modifiedCount > 0) {
+    res.send({ status: "update success", isUpdate: true, response: messageRespone("200") })
   }
-  else{
-    res.send({status:"update failed",isUpdate:false,response:messageRespone("400")})
+  else {
+    res.send({ status: "update failed", isUpdate: false, response: messageRespone("400") })
   }
 })
 module.exports = router;
