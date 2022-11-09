46612f7a6de34842ab4f2ff5754419896fd65500
Shov1bow1 <fireblaster2001@gmail.com>
2022-11-09 21:41:39 +0700

    merge with Nguyen

--- enum/httpCode.js
+++ enum/httpCode.js
@@ -1,11 +1,11 @@
- const HTTPMESSAGE={
-    '404':"Not found",
-    '400':"Bad requetst",
-    '200':'Success',
-    '201':'Created',
-    '202':'Accepted but not been completed',
-    '409':'Account email or username is conflict',
-    '401':"Unauthorized",
-     '403':"Forbidden"
+const HTTPMESSAGE = {
+   '404': "Not found",
+   '400': "Bad requetst",
+   '200': 'Success',
+   '201': 'Created',
+   '202': 'Accepted but not been completed',
+   '409': 'Account email or username is conflict',
+   '401': "Unauthorized",
+   '403': "Forbidden"
 }
 module.exports = { HTTPMESSAGE }
